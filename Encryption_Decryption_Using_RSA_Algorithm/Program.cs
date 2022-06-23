297972b09a2861a7b8f0ec4c0afb6f2a5556fb2f
pranayshinde855 <pranayshinde855@gmail.com>
2022-06-23 10:32:10 +0530

    Add project files.

---
+++ Encryption_Decryption_Using_RSA_Algorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption_Decryption_Using_RSA_Algorithm
+{
+    static class Program
+    {
+        public static string Encrypt(string plainText)
+        {
+            return plainText = Convert.ToBase64String(Encoding.Unicode.GetBytes(plainText));
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            return cipherText = Encoding.Unicode.GetString(Convert.FromBase64String(cipherText));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to encrypt");
+            var plaintext = Console.ReadLine();
+            
+            var cipherText = Encrypt(plaintext);
+            Console.WriteLine("Encrypted string :" + cipherText);
+
+            var plainText2 = Decrypt(cipherText);
+            Console.WriteLine("Decrypted string :" + plainText2);
+        }
+    }
+}
---
+++ Video_Encryption_Decryption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Video_Encryption_Decryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
